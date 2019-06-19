10371ced16a5e997fcf422182c1f711ba8edeeb9
Chris Ball <christopher.ball77@gmail.com>
2019-06-19 11:45:32 +0100

    NHS login - new beta flows

--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,22 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+// Have login
+router.post('/v1/set-up/have-login', function (req, res) {
+
+  // Make a variable and give it the value from 'know-nhs-number'
+  var haveLogin = req.session.data['have-login']
+
+  // Check whether the variable matches a condition
+  if (haveLogin == "Yes"){
+    // Send user to next page
+    res.redirect('login')
+  }
+  else {
+    // Send user to ineligible page
+    res.redirect('create')
+  }
+
+})
+
 module.exports = router;
