572f0f6464794c143ce028d20955b8ce8c37d54c
Will <ws.hepworth@gmail.com>
2019-08-30 11:15:28 +0100

    Created new registration flow

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,1 +1,13 @@
 // ES6 or Vanilla JavaScript
+
+
+// reveal password script
+
+function myFunction() {
+    var x = document.getElementById("revealPassword");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
