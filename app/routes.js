50228bc11bbe84f86832679590e0ae47389ea6e3
Will <ws.hepworth@gmail.com>
2019-12-11 15:23:40 +0000

    Added notify + modified email errors

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,27 @@
+
 // External dependencies
 const express = require('express');
 const router = express.Router();
+const notify = require('notifications-node-client').NotifyClient;
+
+// // Notify integration
+const notifyClient = new notify(process.env.NOTIFYAPIKEY);
+
+// The URL here needs to match the URL of the page that the user is on
+// when they type in their email address
+router.post('/set-up/v4/register-create-password', function (req, res) {
+  if(req.body.userEmail === 'will.hepworth1@nhs.net'){
+    notifyClient.sendEmail('443d169e-9fbb-4fd0-b77e-e57389ed1804',req.body.userEmail)
+    .then(function (response) {
+      res.redirect('/set-up/v4/register-check-email');
+    }).catch(function (error) {
+        console.log('notify-error' + error)
+        res.redirect('/set-up/v4/enter-email');
+      });;
+  }
+  res.redirect('/set-up/v4/register-check-email');
+});
+
 
 // Add your routes here - above the module.exports line
 
@@ -11,7 +32,7 @@ router.post('/v1/set-up/have-login', function (req, res) {
   var haveLogin = req.session.data['have-login']
 
   // Check whether the variable matches a condition
-  if (haveLogin == "Yes"){
+  if (haveLogin == "Yes") {
     // Send user to next page
     res.redirect('login')
   }
@@ -42,7 +63,7 @@ function devModeRoute(req, res, next) {
       console.log('devmode not detected');
     }
   } else {
-    console.log('data found and set to ' +  req.session.data['devMode'] )
+    console.log('data found and set to ' + req.session.data['devMode'])
   }
   next()
 }
