10cc4674e6dbf04e0e1c01c5bf1ac9019014e9b2
Will <ws.hepworth@gmail.com>
2020-02-06 11:59:53 +0000

    Add spinner on landing and forgot password

--- app/routes.js
+++ app/routes.js
@@ -10,33 +10,33 @@ const notifyClient = new notify(process.env.NOTIFYAPIKEY);
 
 // The URL here needs to match the URL of the page that the user is on
 // when they type in their email address
-router.post('/set-up/v4/register-create-password', function (req, res) {
-  //if(req.body.userEmail === 'will.hepworth1@nhs.net'){
-  notifyClient.sendEmail('443d169e-9fbb-4fd0-b77e-e57389ed1804', req.body.userEmail)
-    .then(function (response) {
-      res.redirect('/set-up/v4/register-check-email');
-    }).catch(function (error) {
-      console.log('notify-error' + error)
-      res.redirect('/set-up/v4/enter-email');
-    });;
-  //}
-  //res.redirect('/set-up/v4/register-check-email');
-});
+// router.post('/set-up/v4/register-create-password', function (req, res) {
+//   //if(req.body.userEmail === 'will.hepworth1@nhs.net'){
+//   notifyClient.sendEmail('443d169e-9fbb-4fd0-b77e-e57389ed1804', req.body.userEmail)
+//     .then(function (response) {
+//       res.redirect('/set-up/v4/register-check-email');
+//     }).catch(function (error) {
+//       console.log('notify-error' + error)
+//       res.redirect('/set-up/v4/enter-email');
+//     });;
+//   //}
+//   //res.redirect('/set-up/v4/register-check-email');
+// });
 
-router.post('/set-up/v4/register-enter-phone', function (req, res) {
-  console.log('method called');
-  // if(req.body.phoneNumber === '07429854013'){
-  notifyClient.sendSms('2b7236fc-d18b-493c-807a-c185a2330ee2', req.body.phoneNumber)
-    .then(function (response) {
-      console.log('success')
-      res.redirect('/set-up/v4/register-enter-OTP');
-    }).catch(function (error) {
-      console.log('notify-error' + error)
-      res.redirect('/set-up/v4/register-enter-OTP');
-    });
-  // }
-  //res.redirect('/set-up/v4/register-enter-OTP');
-});
+// router.post('/set-up/v4/register-enter-phone', function (req, res) {
+//   console.log('method called');
+//   // if(req.body.phoneNumber === '07429854013'){
+//   notifyClient.sendSms('2b7236fc-d18b-493c-807a-c185a2330ee2', req.body.phoneNumber)
+//     .then(function (response) {
+//       console.log('success')
+//       res.redirect('/set-up/v4/register-enter-OTP');
+//     }).catch(function (error) {
+//       console.log('notify-error' + error)
+//       res.redirect('/set-up/v4/register-enter-OTP');
+//     });
+//   // }
+//   //res.redirect('/set-up/v4/register-enter-OTP');
+// });
 
 // Routing for mid-level journey
 // router.post('/set-up/v4/auth', function (req, res) {
