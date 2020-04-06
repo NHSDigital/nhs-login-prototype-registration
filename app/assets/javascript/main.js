dfd8519483cbe6e6da170f5f91973569ecb08b62
Suly <suly@hippodigital.co.uk>
2020-04-06 16:21:51 +0100

    Cookie banner implementation
    Adding in cookie banner alraedy implemented in the v1 prototype - the v2 cookies banner has updated content and styling.
    
    This commit just includes the skeleton of the cookie banner (JS and HTML required) - content and styling needs to be applied.

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,46 +1,45 @@
 // reveal password script
 
 function setUpButton(button, input) {
-  var form = document.createElement('form');
-  if ('checkValidity' in form && 'querySelector' in document && 'classList' in document.documentElement) {
+    var form = document.createElement('form');
+    if ('checkValidity' in form && 'querySelector' in document && 'classList' in document.documentElement) {
 
-      // toggle password field between type="text" and type="password"
-      var togglePasswordButton = document.querySelector(button);
+        // toggle password field between type="text" and type="password"
+        var togglePasswordButton = document.querySelector(button);
 
-      if (togglePasswordButton) {
-          var togglePasswordButtonText = togglePasswordButton.querySelector('b');
-          var passwordField = document.querySelector(input);
-          togglePasswordButton.removeAttribute('hidden');
+        if (togglePasswordButton) {
+            var togglePasswordButtonText = togglePasswordButton.querySelector('b');
+            var passwordField = document.querySelector(input);
+            togglePasswordButton.removeAttribute('hidden');
 
 
-          togglePasswordButton.addEventListener('click', function() {
-              var isPressed = JSON.parse(this.getAttribute('aria-pressed'));
+            togglePasswordButton.addEventListener('click', function () {
+                var isPressed = JSON.parse(this.getAttribute('aria-pressed'));
 
-              if (isPressed) {
-                  passwordField.setAttribute('type', 'password');
-                  this.setAttribute('aria-pressed', false);
-                  togglePasswordButtonText.textContent = "Show"
-              } else {
-                  passwordField.setAttribute('type', 'text');
-                  this.setAttribute('aria-pressed', true);
-                  togglePasswordButtonText.textContent = "Hide"
-              }
-          });
-      }
-  }
+                if (isPressed) {
+                    passwordField.setAttribute('type', 'password');
+                    this.setAttribute('aria-pressed', false);
+                    togglePasswordButtonText.textContent = "Show"
+                } else {
+                    passwordField.setAttribute('type', 'text');
+                    this.setAttribute('aria-pressed', true);
+                    togglePasswordButtonText.textContent = "Hide"
+                }
+            });
+        }
+    }
 }
 
 setUpButton('[data-toggle-password]', '[data-toggle-password-field]')
 setUpButton('[data-toggle-password-confirm]', '[data-toggle-password-field-confirm]')
 
-function toggleDevMode(){
+function toggleDevMode() {
     const devModeButton = document.querySelector('#nhsuk-footer ul li a[href*="#"]');
     devModeButton.addEventListener('click', function (event) {
         event.preventDefault();
-        if(window.location.href.indexOf('devMode=') === -1){
-            window.location.href = window.location.href + '?devMode=1'; 
-        }
-        else{
+        if (window.location.href.indexOf('devMode=') === -1) {
+            window.location.href = window.location.href + '?devMode=1';
+        } else {
             window.location.href = '/clear-data';
         }
     }, false);
